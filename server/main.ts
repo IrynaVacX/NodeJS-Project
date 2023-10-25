0fb07d33daec39396295bb1d376d0b30cd34ce68
Iryna <VacX_vb15@student.itstep.org>
2023-10-25 20:10:45 +0200

    main menu chat begin
    some interface and local show messages

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,11 @@
 import { app } from "./server";
 import { connection } from "./db";
+import { Server } from "socket.io";
+
 const PORT = 4000;
+let connect = connection;
 
-
+//////// NAVIGATION ////////
 app.get('/', (req, res) => {
     res.render('main', {
         layout: "index",
@@ -15,7 +18,6 @@ app.get('/menu', (req, res) => {
     })
 });
 
-let connect=connection;
 
 app.get('/registration', (req, res) => {
     res.render('reg', {
@@ -27,6 +29,26 @@ app.get('*', (req, res) => {
     res.render('main');
 });
 
-app.listen(PORT, () => {
+
+//////// SERVER RUN ////////
+const server = app.listen(PORT, () => {
     console.log("Server ready at: ", `http://localhost:${PORT}/`);
+});
+
+////// CREATE SESSION //////
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    // join to global game chat
+    socket.join("main-menu-room");
+    console.log("Connected");
+
+    socket.on("chat message", (msg) => {
+        io.to("main-menu-room").emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        io.to("main-menu-room").emit("chat message", "Disconnected");
+        console.log("Disconnected");
+    });
 });
