8072a9b350723999ae50d99b6b66d3fa6294e8f2
King-Of-Programer <alexdichemix@gmail.com>
2023-11-13 14:42:02 +0200

    login

--- public/js/site.js
+++ public/js/site.js
@@ -6,29 +6,27 @@ document.querySelector('form').addEventListener('submit', function (event) {
     let password = document.getElementById("pass").value;
     let repass = document.getElementById("repass").value;
 
-
     if (action === 'signup') {
-        if (!login || !password /*||!repas */) {
+        if (!login || !password || !repass) {
             alert("All fields are required for Sign up!");
             return;
         }
-        // Поле "Повторите пароль" куда-то исчезло
-        // if (password !== repass) {
-        //     alert("Passwords do not match!");
-        //     return;
-        // }
+        
+        if (password !== repass) {
+            alert("Passwords do not match!");
+            return;
+        }
+        sendRegistrationRequest({ login, password });
     } else if (action === 'signin') {
         if (!login || !password) {
             alert("Nickname and Password are required for Sign in!");
             return;
         }
+        sendLoginRequest({ login, password });
     }
-
-
-    sendDataToServer({ login, password });
 });
 
-function sendDataToServer(data) {
+function sendRegistrationRequest(data) {
     // let data = {
     //     action: action,
     //     nickname: nickname,
@@ -55,14 +53,19 @@ function sendDataToServer(data) {
                     console.log(j);
                     // wrong request data
                     // missing fields: login, password
+                    alert(j.body);
+
                     break;
                 case 403:
                     console.log(j);
+                    
                     if (Array.isArray(j.data)) {
                         let errorMessages = j.data;
                         // errorMessages[0].fieldName
                         // errorMessages[0].errorMessage
+                       
                     }
+                  
                     break;
                 case 500:
                     console.log(j);
@@ -78,4 +81,56 @@ function sendDataToServer(data) {
             console.error('Error:', error);
             alert('An error occurred. Please try again later.');
         });
+}
+
+function sendLoginRequest(data) {
+    // Code to handle login request
+    fetch('/auth', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(j => {
+        /*JSON DATA:
+        -fields: 
+        --status
+        --statusMessage
+        --data is object array({fieldName, errorMessage}) 
+        */
+        switch (j.status) {
+            case 400:
+                console.log(j);
+                // wrong request data
+                // missing fields: login, password
+                alert(j.body);
+
+                break;
+            case 403:
+                console.log(j);
+                
+                if (Array.isArray(j.data)) {
+                    let errorMessages = j.data;
+                    // errorMessages[0].fieldName
+                    // errorMessages[0].errorMessage
+                   
+                }
+              
+                break;
+            case 500:
+                console.log(j);
+                // j.statusMessage
+                break;
+            case 201:
+                console.log(j);
+                //success
+                break;
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('An error occurred. Please try again later.');
+    });
 }
