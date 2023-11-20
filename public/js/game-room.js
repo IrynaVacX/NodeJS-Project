91f431a11c154a48a984ca28b0872abfbf55d661
Iryna <VacX_vb15@student.itstep.org>
2023-11-20 23:32:25 +0200

    fix cat movement

--- public/js/game-room.js
+++ public/js/game-room.js
@@ -1,60 +1,82 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const gameField = document.getElementById('free-move-zone');
+    // const gameField = document.getElementById('free-move-zone');
     const player = document.getElementById('player');
-
-    let posX = 0, posY = 0;
-    const speed = 5;
-    const playerSize = 50; // Измените это значение, если измените размер игрока в CSS
-    const fieldWidth = gameField.clientWidth;
-    const fieldHeight = gameField.clientHeight;
+    let posX = 440, posY = 400;
+    player.style.top = posY + 'px';
+    player.style.left = posX + 'px';
+    
+    const speed = 3;
+    const playerSize = 50;
+    const fieldWidth = 995;
+    const fieldHeight = 910;
     let lastDirection = null;
-    let movementTimer; // Таймер для отслеживания движения
 
     function setPlayerImage(direction) {
         const baseSrc = "assets/cats_stay/";
         const directionSrc = {
-            'left': "left.png", // Предполагается, что это гифка из второй колонки
+            'left': "left.png",
             'right': "right.png",
             'up': "up.png",
             'down': "down.png",
-            
         };
         player.src = baseSrc + directionSrc[direction];
     }
-
-    function movePlayer() {
-        player.style.left = posX + 'px';
-        player.style.top = posY + 'px';
-        clearTimeout(movementTimer);
-        movementTimer = setTimeout(() => {
-            setPlayerImage(lastDirection || 'idle'); // Установка изображения покоя если направление неизвестно
-        }, 500); // Время в мс, после которого игрок переходит в состояние покоя
-    }
-
-    document.addEventListener('keydown', (e) => {
-        clearTimeout(movementTimer); // Остановка таймера при любом нажатии клавиши
-        switch (e.key) {
-            case 'ArrowLeft': // влево
-                posX = Math.max(0, posX - speed);
-                lastDirection = 'left';
-                player.src = "assets/cat/left.gif"; // Предполагается, что это гифка движения влево
-                break;
-            case 'ArrowRight': // вправо
-                posX = Math.min(fieldWidth - playerSize, posX + speed);
-                lastDirection = 'right';
-                player.src = "assets/cat/right.gif";
-                break;
-            case 'ArrowUp': // вверх
-                posY = Math.max(0, posY - speed);
+        
+    let isMoving = {
+        ArrowUp: false,
+        ArrowDown: false,
+        ArrowLeft: false,
+        ArrowRight: false
+    };
+    
+    function moveSquare() {
+        if (isMoving.ArrowUp) {
+            posY = Math.max(0, posY - speed);
+            player.style.top = posY + 'px';
+            if (lastDirection !== 'up') {
                 lastDirection = 'up';
                 player.src = "assets/cat/up.gif";
-                break;
-            case 'ArrowDown': // вниз
-                posY = Math.min(fieldHeight - playerSize, posY + speed);
+            }
+        }
+        if (isMoving.ArrowDown) {
+            posY = Math.min(fieldHeight - playerSize, posY + speed);
+            player.style.top = posY + 'px';
+            if (lastDirection !== 'down') {
                 lastDirection = 'down';
                 player.src = "assets/cat/down.gif";
-                break;
+            }
         }
-        movePlayer();
-    });
+        if (isMoving.ArrowLeft) {
+            posX = Math.max(0, posX - speed);
+            player.style.left = posX + 'px';
+
+            if (lastDirection !== 'left') {
+                lastDirection = 'left';
+                player.src = "assets/cat/left.gif";
+            }
+        }
+        if (isMoving.ArrowRight) {
+            posX = Math.min(fieldWidth - playerSize, posX + speed);
+            player.style.left = posX + 'px';
+            
+            if (lastDirection !== 'right') {
+                lastDirection = 'right';
+                player.src = "assets/cat/right.gif";
+            }
+        }
+        if (!isMoving.ArrowUp && !isMoving.ArrowDown && !isMoving.ArrowLeft && !isMoving.ArrowRight) {
+            setPlayerImage(lastDirection || 'idle');
+        }
+        requestAnimationFrame(moveSquare);
+    }
+    
+      document.addEventListener('keydown', (event) => {
+        isMoving[event.key] = true;
+      });
+    
+      document.addEventListener('keyup', (event) => {
+        isMoving[event.key] = false;
+      });
+    
+    moveSquare();
 });
