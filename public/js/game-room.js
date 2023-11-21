8f739dd7fa95e0ffd5ad0bce5c4bcbe0b272bd9a
King-Of-Programer <alexdichemix@gmail.com>
2023-11-21 20:59:56 +0200

    counter + timer

--- public/js/game-room.js
+++ public/js/game-room.js
@@ -10,7 +10,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const fieldWidth = 1495;
     const fieldHeight = 920;
     let lastDirection = null;
+    let count = 0;
+    let timeLeft = 10 * 60; // 10 минут в секундах
+    const timerElement = document.getElementById('timer');
 
+    function updateTimer() {
+        let minutes = Math.floor(timeLeft / 60);
+        let seconds = timeLeft % 60;
+    
+        // Добавление ведущего нуля к секундам, если они меньше 10
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+    
+        timerElement.innerText = minutes + ":" + seconds;
+        timeLeft--;
+    
+        if (timeLeft < 0) {
+            clearInterval(timerInterval);
+            // Перенаправление на другую страницу
+            window.location.href = 'URL_другой_страницы';
+        }
+    }
+    
+    let timerInterval = setInterval(updateTimer, 1000);
+
+
+    
     function setPlayerImage(direction) {
         const baseSrc = "assets/cats_stay/";
         const directionSrc = {
@@ -28,7 +52,17 @@ document.addEventListener('DOMContentLoaded', () => {
         ArrowLeft: false,
         ArrowRight: false
     };
+
+    function checkCollision(player, element) {
+        const playerRect = player.getBoundingClientRect();
+        const elementRect = element.getBoundingClientRect();
     
+        return !(playerRect.right < elementRect.left || 
+                 playerRect.left > elementRect.right || 
+                 playerRect.bottom < elementRect.top || 
+                 playerRect.top > elementRect.bottom);
+    }
+
     function moveSquare() {
         if (isMoving.ArrowUp) {
             posY = Math.max(0, posY - speed);
@@ -67,9 +101,17 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!isMoving.ArrowUp && !isMoving.ArrowDown && !isMoving.ArrowLeft && !isMoving.ArrowRight) {
             setPlayerImage(lastDirection || 'idle');
         }
-        requestAnimationFrame(moveSquare);
+       document.querySelectorAll('.element').forEach(element => {
+        if (checkCollision(player, element)) {
+            element.remove();
+            count++;
+            document.getElementById('counter').innerText = 'Счетчик: ' + count;
+        }
+    });
+
+    requestAnimationFrame(moveSquare);
     }
-    
+       
       document.addEventListener('keydown', (event) => {
         isMoving[event.key] = true;
       });
